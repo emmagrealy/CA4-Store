3052b50244323f0722bc9ddee6260c6939dff406
emma <emz.grealy@gmail.com>
2020-04-16 16:20:13 +0100

    Initial Commit

---
+++ Store/src/main/java/Controller/ProductController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Home.jsp";
+	}
+
+}
